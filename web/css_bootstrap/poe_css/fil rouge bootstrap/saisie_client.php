0968dc6b1a1cafb2a9edb5e4cf2ae22ae0ba1103
Teddy Ruppin <teddy.ruppin@hotmail.fr>
2020-01-01 18:08:25 +0100

    fil rouge css bootstrap

---
+++ web/css_bootstrap/poe_css/fil rouge bootstrap/saisie_client.php
@@ -0,0 +1,135 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    
+    <!--important pour les medias queries -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!--pour recuperer les extensions bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/menu.css">
+    <link href="https://fonts.googleapis.com/css?family=Courier+Prime&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/png" href="img/marquage-pneu.jpg">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+    <title>garage poe 2019</title>
+    
+  </head>
+  
+  <body>
+  
+  <header class="saisieclient">
+  
+      
+         <button class="bouton1"> <a href="index.html">Accueil</a> </button>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum perferendis voluptatem perspiciatis voluptatibus mollitia dignissimos consequatur totam dolore nulla rem unde, odio eveniet ipsum quis possimus, illum sit porro quisquam.</p>
+      
+
+      <h1> Saisie d'un client </h1>
+      
+        <span class="w3-tag w3-jumbo w3-blue">S</span>
+        <span class="w3-tag w3-jumbo">A</span>
+        <span class="w3-tag w3-jumbo w3-blue">I</span>
+        <span class="w3-tag w3-jumbo">S</span>
+        <span class="w3-tag w3-jumbo w3-blue">I</span>
+        <span class="w3-tag w3-jumbo">E</span>
+        
+        <br/> <br/>
+        
+        <span class="w3-tag w3-xlarge w3-blue">c</span>
+        <span class="w3-tag w3-xlarge">l</span>
+        <span class="w3-tag w3-xlarge w3-blue">i</span>
+        <span class="w3-tag w3-xlarge">e</span>
+        <span class="w3-tag w3-xlarge w3-blue">n</span>
+        <span class="w3-tag w3-xlarge">t</span>
+        
+        <br/> <br/>
+        
+   
+
+
+      <form>
+         
+          <label  for="texte">Nom : </label>
+          <span 
+              class="affichage"> Rentrez une chaine de caractere entre 1 et 50 caractères      </span>   
+          <input id="text" type="text" class="form-control">
+          
+          <br/>
+          
+          <label for="texte">Prenom : </label>
+                    <span 
+              class="affichage"> Rentrez une chaine de caractere entre 1 et 50 caractères      </span>
+          <input id="text" type="text" class="form-control">
+          
+          <br/>
+          
+          <label for="texte">Adresse : </label>
+                    <span 
+              class="affichage"> Rentrez une chaine de caractere entre 1 et 50 caractères      </span>
+          <input id="text" type="text" class="form-control">
+          
+          <br/>
+          
+          <label for="texte">Ville : </label>
+                    <span 
+              class="affichage"> Rentrez une chaine de caractere entre 1 et 50 caractères      </span>
+          <input id="text" type="text" class="form-control">
+          
+          <label for="texte">Code Postal : </label>
+                    <span 
+              class="affichage"> Rentrez une chaine de caractere entre 1 et 50 caractères      </span>
+          <input id="text" type="text" class="form-control">
+          
+          <br/>
+          
+          <label for="texte">Mobile : </label>
+                    <span 
+              class="affichage"> Rentrez une chaine de caractere entre 1 et 50 caractères      </span>
+          <input id="text" type="text" class="form-control">
+          
+          <br/>
+          
+          <label for="texte">Telephone : </label>
+                    <span 
+              class="affichage"> Rentrez une chaine de caractere entre 1 et 50 caractères      </span>
+          <input id="text" type="text" class="form-control">
+          
+          <br/>
+          
+          <button>Envoyer</button>
+        </form>
+        
+        
+
+
+    </header>
+    
+       
+<!--        test php     -->
+       <br/><br/><br/>
+        <form method="GET" action="">
+            <input type="text" name="valeur" />
+            <input type="submit" value="Valider" />
+        </form>
+    
+    <?php 
+      
+      echo $_GET['valeur'];
+      
+      
+      ?>
+      
+    
+    
+    
+    
+    
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/main.js"></script>
+    
+ </body>
+</html>
